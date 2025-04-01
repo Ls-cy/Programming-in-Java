fabe8791f8d6abb24db771055af762fd690ea5ff
Ls-cy <48863495+Ls-cy@users.noreply.github.com>
2025-04-01 12:46:28 -0400

    delete

--- Module 1/WelcometoJava.java
+++
@@ -1,8 +0,0 @@
-public class WelcometoJava{
-  public static void main(String [] args){
-    // Loop runs 5 times
-    for (int i = 1; i <= 5; i++)
-      {System.out.println("Welcome to Java");
-    }
-  }
-}
