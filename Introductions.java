cd9ebbb1eeba9369019988c7bd5985cf05e50443
Ls-cy <48863495+Ls-cy@users.noreply.github.com>
2025-07-21 18:29:05 -0400

    Add files via upload

---
+++ Introductions.java
@@ -0,0 +1,11 @@
+public class Introductions {
+    public static void main(String[] args) {
+       String name = "My name is Lathika Sharma.";
+       String major = "My Major is Cybersecurity.";
+       String hobby = "My are Swimming and dancing. Swimming and dancing help me to stay active and relieve myself of my frustrations.";
+    
+       String introduction = name + major + hobby + "!"; 
+
+    System.out.println(introduction);
+    }
+}
